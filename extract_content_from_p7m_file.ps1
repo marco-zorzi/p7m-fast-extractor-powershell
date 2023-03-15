38e2115494da2e80096c53abd36d30836d06e084
Marco Zorzi <mz.marco.zorzi@gmail.com>
2023-03-15 12:28:23 +0100

    Add comments
    Add comments

--- extract_content_from_p7m_file.ps1
+++ extract_content_from_p7m_file.ps1
@@ -26,22 +26,50 @@ File: extract_content_from_p7m_file.ps1
 
 function extract_content_from_p7m_file($folder_p7m, $folder_extract)
 {
+    # Add the 'System.IO' and 'System.Security' assemblies
     Add-Type -AssemblyName System.IO
     Add-Type -AssemblyName System.Security
+    
+    # Get a list of files in the '$folder_p7m' directory
     $files = Get-ChildItem $folder_p7m
+    
+    # Initialize a counter variable to keep track of progress
     $i = 0
+    
+    # Iterate over each file in the directory
     foreach ($file in $files) {
+        # Increment the counter variable
         $i++
+        
+        # Display a progress bar indicating the current file being processed
         Write-Progress -Activity "Content extraction from $file" -Status "File $($i) of $($($files).count)" -PercentComplete (($i / $($files).count) * 100)
+        
+        # Get the full path to the file
         $file_path = $file.FullName
+       
+        # Get the base name of the file
         $file_basename = $file.Basename
+        
+        # Read the contents of the file into a byte array
         $content = [IO.File]::ReadAllBytes("$file_path")
+        
+        # Create a new 'SignedCms' object and decode the contents of the file
         $signedCms = New-Object System.Security.Cryptography.Pkcs.SignedCms
         $signedCms.Decode($content)
+        
+        # Clear the 'content' variable to free up memory
         Clear-Variable content
+        
+        # Get the content of the signed CMS message
         $infoFile = $signedCms.ContentInfo.Content
+        
+        # Clear the 'signedCms' variable to free up memory
         Clear-Variable signedCms
+        
+        # Write the content of the message to a file in the '$folder_extract' directory
         [IO.File]::WriteAllBytes("$folder_extract\$file_basename", $infoFile)
+        
+        # Clear the 'infoFile' variable to free up memory
         Clear-Variable infoFile
     }
 }
